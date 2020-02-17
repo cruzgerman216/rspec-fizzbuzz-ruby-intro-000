3e3a6a8ba1c187e7ff856f15f1be337eab06d00e
German Cruz <cruzgerman216@yahoo.com>
2020-02-17 21:10:17 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
   def fizzbuzz(int)
     if int % 3 == 0 # if the number int is divisible by 3
       "Fizz" # Go fizz
-else if int % 5 == 0
+elseif int % 5 == 0
   "Buzz"
-else if int % 15 == 0
+elseif int % 15 == 0
   "fizzbuzz"
 else
   nil
