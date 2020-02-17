8ac45f8e1b69b3c7561e3e6d771962ebaf471af5
German Cruz <cruzgerman216@yahoo.com>
2020-02-17 21:09:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-
-end
+  def fizzbuzz(int)
+    if int % 3 == 0 # if the number int is divisible by 3
+      "Fizz" # Go fizz
+else if int % 5 == 0
+  "Buzz"
+else if int % 15 == 0
+  "fizzbuzz"
+else 
+  nil
+    end
+  end
